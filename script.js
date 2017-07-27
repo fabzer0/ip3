64308921d536bddd74df82bb2e1491fc29c00d87
fabischapeli <fabischapeli97@gmail.com>
2017-07-27 19:43:29 +0300

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   $("#one form").submit(function(event) {
     var numberInput = $("input#number").val();
-
+    var numberInput = [0, 20];
     $("ul").text(numberInput);
+
 
     $("#two").show();
 
     event.preventDefault();
+
   });
 });
