e7752231390a661a855c1313949f454eb3038e94
fabischapeli <fabischapeli97@gmail.com>
2017-10-19 08:19:18 +0300

    separated user-interface code from business logic code in two two files

---
+++ js/pingpong-interface.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
--- js/pingpong.js
+++ js/pingpong.js
@@ -13,14 +13,3 @@ function pingPong(goal) {
   }
   return output;
 }
-
-$(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
-  });
-});
