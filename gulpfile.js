5a8946ed6ba13be77ac61a49a44a7fcb5d20cbf6
fabischapeli <fabischapeli97@gmail.com>
2017-11-04 09:28:39 +0300

    created the first gulp task and loaded the page to the browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
-var gulp = require("gulp");
-
-gulp.task('myTask', function() {
-  console.log('hello gulp');
-
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/pingpong-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
