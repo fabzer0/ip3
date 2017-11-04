3508a7f74afe4a9155660048fa8802331acf4396
fabischapeli <fabischapeli97@gmail.com>
2017-11-04 10:22:25 +0300

    created a minifyScript gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
@@ -14,4 +15,9 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
 });
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -10,3 +10,12 @@ $(document).ready(function() {
     });
   });
 });
+
+$(document).ready(function() {
+  $('#signup').submit(function(event) {
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + 'has been added to our list!</p>');
+  });
+});
