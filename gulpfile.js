3090e6f52f0b84af72ff23419f5adcede4db62fa
fabischapeli <fabischapeli97@gmail.com>
2017-11-05 08:48:48 +0300

    installed and created bower tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,17 @@ var uglify = require('gulp-uglify');
 var buildProduction = utilities.env.production;
 var del = require('del');
 var jshint = require('gulp-jshint');
+var lib = require('bower-files') ({
+  "overrides":{
+    "bootstrap":{
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
 
 gulp.task('concatInterface', function() {
@@ -34,9 +45,22 @@ gulp.task("build", ['clean'], function() {
   } else {
     gulp.start('jsBrowserify');
   }
+  gulp.start('bower');
 });
 gulp.task('jshint', function() {
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
+gulp.task('bowerJS', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+gulp.task('bowerCSS', function() {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
