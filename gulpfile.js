837b28ce58782f52e8e5b3c5cce624efc5bad161
fabischapeli <fabischapeli97@gmail.com>
2017-11-05 11:04:12 +0300

    introduction to API wether responses and JSON

--- gulpfile.js
+++ gulpfile.js
@@ -74,10 +74,14 @@ gulp.task('serve', function() {
   });
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
 });
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function() {
   browserSync.reload();
 });
 gulp.task('bowerBuild', ['bower'], function() {
+  browserSync.reload();
+});
+gulp.task('htmlBuild', function() {
   browserSync.reload();
 });
---
+++ js/weather-interface.js
@@ -0,0 +1,11 @@
+var apiKey = "";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The Humidity in" + city + "is" + response.main.humidity + "%");
+    });
+  });
+});
